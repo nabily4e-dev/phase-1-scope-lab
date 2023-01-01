172b9be9407f06967c44126b0ec46619da406db7
nabily4e-dev <nbl.dev21@gmail.com>
2023-01-01 16:38:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBest) {
+  window.bestCustomer = newBest;
+}
+
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.');
+}
